397e00765b68b7c34fdbc01a4840a34719071f41
Val <knigochei3333@gmail.com>
2026-03-14 14:41:27 +0300

    Создал Spring Boot проект: web, jpa, thymeleaf, postgresql

---
+++ src/test/java/com/val/studynotes/StudynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.val.studynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
