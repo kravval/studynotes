12c007c274af2798304077f9fefe837d37482ab1
Val <knigochei3333@gmail.com>
2026-03-18 20:26:29 +0300

    Добавил страницу списка заметок: Thymeleaf + CSS

---
+++ src/main/java/com/val/studynotes/controller/NoteWebController.java
@@ -0,0 +1,23 @@
+package com.val.studynotes.controller;
+
+import com.val.studynotes.dto.NoteResponse;
+import com.val.studynotes.service.NoteService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class NoteWebController {
+    private final NoteService noteService;
+
+    public NoteWebController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/notes")
+    public String listNotes(Model model) {
+        List<NoteResponse> notes = noteService.getAllNotes();
+        model.addAttribute("notes", notes);
+        return "note-list";
+    }
+}
