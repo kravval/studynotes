819a47d617d70631ba24adf448ffc4fe70c94125
Val <knigochei3333@gmail.com>
2026-03-19 11:21:18 +0300

    Добавил аннотацию

--- src/main/java/com/val/studynotes/controller/NoteWebController.java
+++ src/main/java/com/val/studynotes/controller/NoteWebController.java
@@ -2,11 +2,13 @@ package com.val.studynotes.controller;
 
 import com.val.studynotes.dto.NoteResponse;
 import com.val.studynotes.service.NoteService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Controller
 public class NoteWebController {
     private final NoteService noteService;
 
