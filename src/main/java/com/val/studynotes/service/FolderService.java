80a1570285b373ad68fc23561829ff25336e424d
Val <knigochei3333@gmail.com>
2026-03-21 20:11:47 +0300

    Создал FolderService

--- src/main/java/com/val/studynotes/service/FolderService.java
+++ src/main/java/com/val/studynotes/service/FolderService.java
@@ -1,4 +1,20 @@
 package com.val.studynotes.service;
 
+import com.val.studynotes.model.Folder;
+import com.val.studynotes.repository.FolderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FolderService {
+    private final FolderRepository folderRepository;
+
+    public FolderService(FolderRepository folderRepository) {
+        this.folderRepository = folderRepository;
+    }
+
+    public List<Folder> getRootFolders() {
+        return folderRepository.findByParentIsNull();
+    }
 }
