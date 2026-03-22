388996398efb7cc89b14e87e819e9bdade744df8
Val <knigochei3333@gmail.com>
2026-03-22 16:45:43 +0300

    Исправил именование поля Id → id в Entity Note

--- src/main/java/com/val/studynotes/model/Note.java
+++ src/main/java/com/val/studynotes/model/Note.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
     @Column(nullable = false)
     private String title;
@@ -33,11 +33,11 @@ public class Note {
     }
 
     public Long getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Long id) {
-        Id = id;
+        this.id = id;
     }
 
     public String getTitle() {
